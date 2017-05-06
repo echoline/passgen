afa7f25eec41c72ebcc1daf779a56146c0c923ff
eli <eli@darkstar>
2017-05-06 16:33:00 -0700

    more portability

--- alnum.c
+++ alnum.c
@@ -1,12 +1,10 @@
-#include <stdio.h>
-#include <ctype.h>
-
 int main() {
 	char buf[1];
 	while (read(0, buf, 1) == 1) {
-		if (isalnum(buf[0])) {
+		if ((buf[0] >= 'A' && buf[0] <= 'Z')
+		  ||(buf[0] >= 'a' && buf[0] <= 'z')
+		  ||(buf[0] >= '0' && buf[0] <= '9')) {
 			write (1, buf, 1);
-			fflush (stdout);
 		}
 	}
 }
